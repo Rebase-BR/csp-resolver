36a25221bf2d4f96bb84351cf193713f9c4d96aa
gustavo.costa <gustavoalberttodev@gmail.com>
2024-03-28 16:17:12 -0300

    add missing constraint test

--- spec/csp/problems/sculpture/constraints/room_limit_to_one_constraint_spec.rb
+++ spec/csp/problems/sculpture/constraints/room_limit_to_one_constraint_spec.rb
@@ -26,9 +26,22 @@ RSpec.describe CSP::Problems::Sculpture::RoomLimitToOneConstraint do
         variables = [variable, variable2, variable3]
         constraint = described_class.new(room: 1, variables:)
 
-        satisfies = constraint.satisfies?({ variable => 3, variable2 => 2, variable3 => 2 })
+        satisfies = constraint.satisfies?({ variable => 3, variable2 => 2, variable3 => 2})
 
         expect(satisfies).to eq true
+      end
+    end
+
+    context 'when atleast two variables in the assigment has the room value' do
+      it 'retuns false' do
+        variable = double('Variable')
+        variable2 = double('Variable')
+        variable3 = double('Variable')
+        variables = [variable, variable2, variable]
+        constraint = described_class.new(room: 1, variables:)
+        satisfies = constraint.satisfies?({ variable => 1, variable2 => 1, variable3 => 2 })
+
+        expect(satisfies).to eq false
       end
     end
   end
