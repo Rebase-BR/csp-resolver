b94fc78667f8eee19c72a701d551086628305579
gustavo.costa <gustavoalberttodev@gmail.com>
2024-04-08 15:07:09 -0300

    remove examples from csp

--- lib/csp.rb
+++ lib/csp.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'csp/version'
-require_relative '../examples/sculpture'
-require_relative '../examples/event_scheduling'
-require_relative '../examples/map_coloring'
-require_relative '../examples/queen'
 require_relative 'csp/constraint'
 require_relative 'csp/problem'
 
